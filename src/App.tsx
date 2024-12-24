558f10cde94cdd2a1bbb19967bc7b0a45358116b
alaminedione <alaminedione@proton.me>
2024-12-24 22:52:33 +0000

    ajouter le fonction search et corriger le nom de certaines attributs

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ import { Button } from './components/ui/button';
 
 const App = () => {
 
-  const [exampleTagsFileType, setExampleTagsFileType] = useState<Tag[]>(tagsExampleFileType);
-  const [exampleTagsSitesToExclude, setExampleTagsSitesToExclude] = useState<Tag[]>(tagsExampleSitesToExclude);
-  const [exampleTagsWords, setExampleTagsWords] = useState<Tag[]>(tagsExampleWords);
-  const [exampleTagsWordsInTitle, setExampleTagsWordsInTitle] = useState<Tag[]>(tagsExampleWordsInTitle);
-  const [exampleTagsWordsInUrl, setExampleTagsWordsInUrl] = useState<Tag[]>(tagsExampleWordsInUrl);
+  const [TagsFileType, setExampleTagsFileType] = useState<Tag[]>(tagsFileType);
+  const [TagsSitesToExclude, setExampleTagsSitesToExclude] = useState<Tag[]>(tagsSitesToExclude);
+  const [TagsWords, setExampleTagsWords] = useState<Tag[]>(tagsWords);
+  const [TagsWordsInTitle, setExampleTagsWordsInTitle] = useState<Tag[]>(tagsWordsInTitle);
+  const [TagsWordsInUrl, setExampleTagsWordsInUrl] = useState<Tag[]>(tagsWordsInUrl);
   // const [exampleTagsWordsInText, setExampleTagsWordsInText] = useState<Tag[]>(tagsExampleWordsInText);
-  const [exampleTagsSites, setExampleTagsSites] = useState<Tag[]>(tagsExampleSites);
+  const [TagsSites, setExampleTagsSites] = useState<Tag[]>(tagsSites);
   const [searchText, setSearchText] = useState("");
 
 
@@ -30,6 +30,22 @@ const App = () => {
     setExampleTagsSitesToExclude([])
   }
 
+  function search() {
+    const sitesIncluded = TagsSites.map(tag => `site:${tag.text}`).join(' OR ')
+    const excludeSites = TagsSitesToExclude.map(tag => `-site:${tag.text}`).join(' OR ')
+    const wodsExcluded = TagsWords.map(tag => `-"${tag.text}"`).join(' ')
+    const fileTypes = TagsFileType.map(tag => `filetype:${tag.text}`).join(' OR ')
+    const wodsInTitle = 'intitle:' + TagsWordsInTitle.map(tag => `"${tag.text}"`).join(' ')
+    const wodsInUrl = "inurl:" + TagsWordsInUrl.map(tag => `"${tag.text}"`).join(' ')
+    // const wodsInText = exampleTagsWordsInText.map(tag => `intext:${tag.text}`).join(' ')
+
+    const queryParams = [sitesIncluded, excludeSites, fileTypes, wodsExcluded, wodsInTitle, wodsInUrl].filter(param => param)
+    const queryString = queryParams.join(' ')
+    const encodedQueryString = encodeURIComponent(queryString)
+    const searchUrl = `https://duckduckgo.com/?q=${encodedQueryString}`
+    window.open(searchUrl, '_blank')
+  }
+
   return (
     <>
       <header className="flex items-center justify-center">
@@ -38,17 +54,17 @@ const App = () => {
 
 
       <div className="options flex flex-col gap-1 mt-3">
-        <InputTags id='websites' label='websites' placeholder='sur quels websites ou noms de domaines tu veux faire les recherches' exampleTags={exampleTagsSites} setExampleTags={setExampleTagsSites} />
-        <InputTags id='site-toExclude' label='ajouter des sites a exclure' placeholder='ajouter des sites a exclure  ' exampleTags={exampleTagsSitesToExclude} setExampleTags={setExampleTagsSitesToExclude} />
-        <InputTags id='word-toExclude' label='ajouter des mots a exclure' placeholder='ajouter des mots a exclure' exampleTags={exampleTagsWords} setExampleTags={setExampleTagsWords} />
-        <InputTags id='filetypes' label='ajouter les types de fichiers' placeholder="types de fichier example: pdf, docx, mp4, png" exampleTags={exampleTagsFileType} setExampleTags={setExampleTagsFileType} />
-        <InputTags id="intitle" label="intitle" placeholder="mots qui doivent apparaître dans le titre" exampleTags={exampleTagsWordsInTitle} setExampleTags={setExampleTagsWordsInTitle} />
-        <InputTags id="inurl" label="inurl" placeholder="mots qui doivent apparaître dans l'url" exampleTags={exampleTagsWordsInUrl} setExampleTags={setExampleTagsWordsInUrl} />
+        <InputTags id='websites' label='websites' placeholder='sur quels websites ou noms de domaines tu veux faire les recherches' tags={TagsSites} setTags={setExampleTagsSites} />
+        <InputTags id='site-toExclude' label='ajouter des sites a exclure' placeholder='ajouter des sites a exclure  ' tags={TagsSitesToExclude} setTags={setExampleTagsSitesToExclude} />
+        <InputTags id='word-toExclude' label='ajouter des mots a exclure' placeholder='ajouter des mots a exclure' tags={TagsWords} setTags={setExampleTagsWords} />
+        <InputTags id='filetypes' label='ajouter les types de fichiers' placeholder="types de fichier example: pdf, docx, mp4, png" tags={TagsFileType} setTags={setExampleTagsFileType} />
+        <InputTags id="intitle" label="intitle" placeholder="mots qui doivent apparaître dans le titre" tags={TagsWordsInTitle} setTags={setExampleTagsWordsInTitle} />
+        <InputTags id="inurl" label="inurl" placeholder="mots qui doivent apparaître dans l'url" tags={TagsWordsInUrl} setTags={setExampleTagsWordsInUrl} />
         {/* <InputTags id="intext" label="intext" placeholder="mots qui doivent apparaître dans le texte" exampleTags={exampleTagsWordsInText} setExampleTags={setExampleTagsWordsInText} /> */}
         <Button variant={'outline'} className='w-64 ml-auto' onClick={deleteAllTags}>effacer tout </Button>
       </div>
       <div className='flex justify-between items-center'>
-        <ShimmerButton className='flex  w-92'>I'm feeling lucky</ShimmerButton>
+        <ShimmerButton className='flex  w-92' onClick={search}>I'm feeling lucky</ShimmerButton>
         <DockBottom />
 
       </div>
@@ -57,7 +73,7 @@ const App = () => {
 
   )
 }
-const tagsExampleSites = [
+const tagsSites = [
   {
     id: "1",
     text: "example.com",
@@ -71,7 +87,7 @@ const tagsExampleSites = [
     text: "good.com",
   },
 ];
-const tagsExampleWords = [
+const tagsWords = [
   {
     id: "1",
     text: "lucky",
@@ -85,7 +101,7 @@ const tagsExampleWords = [
     text: "awesome",
   }
 ];
-const tagsExampleFileType = [
+const tagsFileType = [
   {
     id: "1",
     text: "pdf",
@@ -99,7 +115,7 @@ const tagsExampleFileType = [
     text: "png",
   }
 ];
-const tagsExampleWordsInTitle = [
+const tagsWordsInTitle = [
   {
     id: "1",
     text: "beautiful",
@@ -113,7 +129,7 @@ const tagsExampleWordsInTitle = [
     text: "awesome",
   }
 ];
-const tagsExampleWordsInUrl = [
+const tagsWordsInUrl = [
   {
     id: "1",
     text: "admin",
@@ -128,7 +144,7 @@ const tagsExampleWordsInUrl = [
   }
 ];
 
-const tagsExampleWordsInText = [
+const tagsWordsInText = [
   {
     id: "1",
     text: "cool",
@@ -143,7 +159,7 @@ const tagsExampleWordsInText = [
   }
 ];
 
-const tagsExampleSitesToExclude = [
+const tagsSitesToExclude = [
   {
     id: "1",
     text: "example.com",
