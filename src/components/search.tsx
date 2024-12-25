22f8783970141d0a24f542135fe8525bf87fa7a8
alaminedione <alaminedione@proton.me>
2024-12-25 09:44:18 +0000

    fonctionalite de recherche avec IA

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,8 @@
+import { Loader2 } from "lucide-react";
 import { PlaceholdersAndVanishInput } from "../components/ui/placeholders-and-vanish-input";
-import TypingAnimation from "@/components/ui/typing-animation";
+import TypingAnimation from "../components/ui/typing-animation";
 import { RainbowButton } from "./ui/rainbow-button";
+import { useState } from "react";
 
 type SearchBarProps = {
   setSearchText: React.Dispatch<React.SetStateAction<string>>;
@@ -15,15 +17,68 @@ export function SearchBar(
     "Feel free to ask anything",
     "Hope you like Deep Search",
   ];
+  const tokens = `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`
+
+  async function fetchQuery(query: string) {
+    return await fetch("https://openrouter.ai/api/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${tokens}`,
+        // "HTTP-Referer": `${YOUR_SITE_URL}`, // Optional, for including your app on openrouter.ai rankings.
+        // "X-Title": `${YOUR_SITE_NAME}`, // Optional. Shows in rankings on openrouter.ai.
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        // "model": "meta-llama/llama-3.1-70b-instruct:free",
+        "model": "google/gemma-2-9b-it:free",
+        "messages": [
+          {
+            "role": "user",
+            "content":
+              `
+              - requête : ${query}
+              - élabore une requête pertinente en utilisant les Google Dorks
+              - fournis uniquement la requête que tu as générée 
+              `
+          }
+        ]
+      })
+    });
+  }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-    setSearchText(searchText);
+    setSearchText(e.target.value);
   };
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  function Submit() {
+    console.log("onSubmit: searchText ", searchText);
+    if (!searchText) {
+      return;
+    }
+
+    setIsSubmitting(true);
+    fetchQuery(searchText)
+      .then(response => response.json(), console.error)
+      .then(data => {
+        let content
+        if (data.choices && data.choices.length > 0) {
+          content = data.choices[0]?.message.content
+        } else if (data.message && data.message.content) {
+          content = data.message.content
+        }
+        if (content) {
+          console.log(content)
+          content = encodeURIComponent(content)
+          window.open(`https://duckduckgo.com/?q=${content}`, '_blank')
+        }
+      })
+      .finally(() => setIsSubmitting(false));
+  }
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    window.open(`https://www.duckduckgo.com/?q=${searchText}`, "_blank");
+    Submit();
   };
 
   return (
@@ -35,9 +90,18 @@ export function SearchBar(
           onChange={handleChange}
           onSubmit={onSubmit}
         />
-        <RainbowButton className="w-64 "> search with AI</RainbowButton>
+        <RainbowButton className="w-64 " onClick={Submit}> search with AI
+
+          {isSubmitting && (
+            <span >
+              <Loader2 className="ml-2 animate-spin" />
+            </span>
+          )}
+        </RainbowButton>
       </div>
     </div>
-
   );
 }
+
+
+
