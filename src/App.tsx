7beb9c56d04e3d8a045be04ba1b89ffae5ad204d
alaminedione <alaminedione@proton.me>
2024-12-26 10:28:55 +0000

    remove label

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState, useEffect } from "react";
-import InputTags from '@/components/input-tags' 
+import InputTags from '@/components/input-tags'
 import { Tag } from "emblor";
 import { SearchBar } from './components/search';
 import { DockBottom } from '@/components/dock';
@@ -117,12 +117,12 @@ const App = () => {
 
       </header>
       <div className="options flex flex-col gap-3">
-        <InputTags id='websites' label='websites' placeholder='sur quels websites ou noms de domaines tu veux faire les recherches' tags={TagsSites} setTags={setExampleTagsSites} />
-        <InputTags id='site-toExclude' label='ajouter des sites a exclure' placeholder='ajouter des sites a exclure  ' tags={TagsSitesToExclude} setTags={setExampleTagsSitesToExclude} />
-        <InputTags id='word-toExclude' label='ajouter des mots a exclure' placeholder='ajouter des mots a exclure' tags={TagsWords} setTags={setExampleTagsWords} />
-        <InputTags id='filetypes' label='ajouter les types de fichiers' placeholder="types de fichier example: pdf, docx, mp4, png" tags={TagsFileType} setTags={setExampleTagsFileType} />
-        <InputTags id="intitle" label="intitle" placeholder="mots qui doivent apparaître dans le titre" tags={TagsWordsInTitle} setTags={setExampleTagsWordsInTitle} />
-        <InputTags id="inurl" label="inurl" placeholder="mots qui doivent apparaître dans l'url" tags={TagsWordsInUrl} setTags={setExampleTagsWordsInUrl} />
+        <InputTags id='websites' placeholder='sur quels websites ou noms de domaines tu veux faire les recherches' tags={TagsSites} setTags={setExampleTagsSites} />
+        <InputTags id='site-toExclude' placeholder='ajouter des sites a exclure  ' tags={TagsSitesToExclude} setTags={setExampleTagsSitesToExclude} />
+        <InputTags id='word-toExclude' placeholder='ajouter des mots a exclure' tags={TagsWords} setTags={setExampleTagsWords} />
+        <InputTags id='filetypes' placeholder="types de fichier example: pdf, docx, mp4, png" tags={TagsFileType} setTags={setExampleTagsFileType} />
+        <InputTags id="intitle" placeholder="mots qui doivent apparaître dans le titre" tags={TagsWordsInTitle} setTags={setExampleTagsWordsInTitle} />
+        <InputTags id="inurl" placeholder="mots qui doivent apparaître dans l'url" tags={TagsWordsInUrl} setTags={setExampleTagsWordsInUrl} />
         <div className="flex  items-center mt-3  gap-3">
           <Button className="ml-1 w-min" onClick={search}>
             search
