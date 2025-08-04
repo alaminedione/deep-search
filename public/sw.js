5e17be2873157027a7d2d711a3483c56a008909b
alamine <alaminedione@proton.me>
2025-08-04 22:59:53 +0000

    feat: installable

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.svg',
+        '/manifest.json',
+        '/src/App.css',
+        '/src/index.css',
+        '/src/main.tsx',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
